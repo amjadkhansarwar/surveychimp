31eb858bfc1696e06448a727a80f847734a8299a
Rashadul Haque <mohammed.haque@iths.se>
2022-11-09 12:34:54 +0100

    Added rating stars and thank you page

---
+++ apps/surveychimp-survey-spa/src/app/pages/ThankYou.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const ThankYou = ()=>{
+    return(
+        <div style={{display:'flex',justifyContent:'center', alignItems:'center', background:'hotpink',height:'100vh'}}>
+            <h1 style={{color:"#fff", fontWeight:"bold" ,fontSize:"1.5rem"}}>Thank you for your feedback!</h1>
+        </div>
+    )
+}
+
+export default ThankYou
