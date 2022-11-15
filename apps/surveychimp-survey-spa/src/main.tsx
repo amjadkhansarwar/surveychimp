25e75cdd3a483c85c83ed0d239657da64b119c0f
Apple <apple@Apples-MacBook-Air.local>
2022-11-15 15:12:05 +0100

    Now sentry is working

--- apps/surveychimp-survey-spa/src/main.tsx
+++ apps/surveychimp-survey-spa/src/main.tsx
@@ -1,8 +1,19 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 
 import App from './app/app';
+Sentry.init({
+    dsn: "https://ad1b37e0c05f411bbef43f31ff8ce73a@o4504162652258304.ingest.sentry.io/4504162654355456",
+    integrations: [new BrowserTracing()],
+  
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
--- libs/surveychimp-lib/src/lib/clients/mongo/survey/getSurveyById.ts
+++ libs/surveychimp-lib/src/lib/clients/mongo/survey/getSurveyById.ts
@@ -5,20 +5,14 @@ export const getSurveyById = async (surveyId: string) => {
     if (!survey) {
         throw "404";
     }
-
-
     return survey;
 }
 
 
 export const getSurveyAll = async () => {
-    const surveyAll = SurveyModel.find({}).exec();
-    console.log(surveyAll);
-
+    const surveyAll = SurveyModel.find({});
     if (!surveyAll) {
-        throw "404";
+        throw '404';
     }
-    
-
     return surveyAll;
 }
